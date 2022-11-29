93670ce71da2f6f47732eb9e6de93e53d8f098a9
Michel Nekrasov <moscou@inbox.ru>
2022-11-29 19:06:50 +0300

    add Zadacha 66 corrected

--- Zadacha_66/Program.cs
+++ Zadacha_66/Program.cs
@@ -4,3 +4,28 @@
 // M = 4; N = 8. -> 30
 
 
+int m = InputNumbers("Введите значение m: ");
+int n = InputNumbers("Введите значение n: ");
+int temp = m;
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+PrintSumm(m, n, temp=0);
+void PrintSumm(int m, int n, int summ)
+{
+  summ = summ + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма натуральных элементов в промежутке от m до n = {summ}. ");
+    return;
+  }
+  PrintSumm(m, n - 1, summ);
+}
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
